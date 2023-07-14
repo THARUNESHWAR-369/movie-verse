a83cbdfbc9d490c5352c2f904c169b745d4f9a7a
THARUNESHWAR-369 <s.tharuneshwar.369@gmail.com>
2023-07-14 12:42:27 +0530

    yml updated

--- movieverse/src/components/pages/home/homePage.js
+++ movieverse/src/components/pages/home/homePage.js
@@ -14,9 +14,7 @@ export const HomePage = () => {
   const [bgImg, setBgImg] = useState("");
   const [theme, setTheme] = useState("");
 
-  console.log("process.env.REACT_APP_API_SERVICE_GET_MOVIE_NAME_LIST_URL: ",config.REACT_APP_API_SERVICE_GET_MOVIE_NAME_LIST_URL, config.REACT_APP_API_SERVICE_GET_MOVIE_DETAILS_URL);
-
-
+  console.log("process.env.REACT_APP_API_SERVICE_GET_MOVIE_NAME_LIST_URL: ",config.REACT_APP_API_SERVICE_GET_MOVIE_NAME_LIST_URL, config.REACT_APP_API_SERVICE_GET_MOVIE_DETAILS_URL, config.REACT_APP_API_SERVICE_GET_POPULAR_MOVIE_URL);
 
   const fetchNowPlayingMovieData = async () => {
     setLoadingText("Loading...");
@@ -81,6 +79,7 @@ export const HomePage = () => {
   
   useEffect(() => {
     window.scrollTo(0, 0);
+    console.log("process.env.REACT_APP_API_SERVICE_GET_MOVIE_NAME_LIST_URL: ",config.REACT_APP_API_SERVICE_GET_MOVIE_NAME_LIST_URL, config.REACT_APP_API_SERVICE_GET_MOVIE_DETAILS_URL, config.REACT_APP_API_SERVICE_GET_POPULAR_MOVIE_URL);
     fetchNowPlayingMovieData();
   },[]);
 
